3db532d72c1e3a40fe8da430f756667a2c84e88f
nevaskpa <54648863+nevaskpa@users.noreply.github.com>
2019-12-02 16:44:46 -0500

    Consume JSON from other group.

---
+++ CafeRecords/BusinessOwner.cs
@@ -0,0 +1,61 @@
+﻿namespace BusinessOwnerRecord
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class BusinessOwner
+    {
+        [JsonProperty("accountNumber")]
+        public string AccountNumber { get; set; }
+
+        [JsonProperty("doingBusinessAsName")]
+        public string DoingBusinessAsName { get; set; }
+
+        [JsonProperty("ownerFirstName")]
+        public string OwnerFirstName { get; set; }
+
+        [JsonProperty("ownerLastName")]
+        public string OwnerLastName { get; set; }
+
+        [JsonProperty("ownerTitle")]
+        public string OwnerTitle { get; set; }
+
+        [JsonProperty("licenseNumber")]
+        public string LicenseNumber { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("businessActivity")]
+        public string BusinessActivity { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+    }
+
+    public partial class BusinessOwner
+    {
+        public static BusinessOwner[] FromJson(string json) => JsonConvert.DeserializeObject<BusinessOwner[]>(json, BusinessOwnerRecord.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this BusinessOwner[] self) => JsonConvert.SerializeObject(self, BusinessOwnerRecord.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
---
+++ CafeRecords/Pages/SearchBusiness.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using BusinessOwnerRecord;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CafeRecords.Pages
+{
+    public class SearchBusinessModel : PageModel
+    {
+        public void OnGet()
+        {
+            SearchCompleted = false;
+        }
+
+        [BindProperty]
+        public string Search { get; set; }
+        public bool SearchCompleted { get; set; }
+
+        public ICollection<BusinessOwner> businessOwners { get; set; }
+
+        public void OnPost()
+        {
+            string ownerJsonString = GetData("https://licenseowners2019.azurewebsites.net/Privacy");
+            businessOwners = BusinessOwner.FromJson(ownerJsonString);
+            ViewData["allBusinessOwners"] = businessOwners;
+
+            businessOwners = businessOwners.Where(x => x.DoingBusinessAsName == Search).ToArray();
+            SearchCompleted = true;
+        }
+        public string GetData(string endpoint)
+        {
+            string downloadedJson;
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedJson = webClient.DownloadString(endpoint);
+            }
+            return downloadedJson;
+        }
+    }
+}
